81c0a553cd1ff13d39b1a9394036910a22ab4897
wu-yiqiang <wu_yiqiang@outlook.com>
2024-07-01 00:13:29 +0800

    增加ORM支持

--- app/user/models.py
+++ app/user/models.py
@@ -1,10 +1,27 @@
 from tortoise import fields
 from tortoise.models import Model
+
 from app.common.db import BaseModel
 
-class Users(Model, BaseModel):
+class User(Model, BaseModel):
     email = fields.CharField(max_length=255, unique=True)
     password = fields.CharField(max_length=255)
 
     class Meta:
-        table = 'users'
+        table = 'user'
+
+class Teacher(Model, BaseModel):
+    name = fields.CharField(max_length=255, unique=True)
+    gender = fields.BooleanField(max_length=255, null=False)
+    teacher_id = fields.IntField(null=False)
+
+    class Meta:
+        table = 'teacher'
+
+
+class Course(Model, BaseModel):
+    name = fields.CharField(max_length=255, unique=True)
+    password = fields.CharField(max_length=255)
+    course_id = fields.IntField(null=False)
+    class Meta:
+        table = 'course'
--- main.py
+++ main.py
@@ -2,12 +2,21 @@ import uvicorn
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 from starlette.staticfiles import StaticFiles
-
+from tortoise.contrib.fastapi import register_tortoise
+from settings import  TORTOISE_ORM
 from router import routers
 
 # 实例化
 app = FastAPI()
+register_tortoise(#这里是启动app的，之后会考虑和使用uvicorn启动的性能差别
+    app,
+    config=TORTOISE_ORM,
+    # generate_schemas=True,#如果数据库为空，则自动生成对应表单,生产环境不要开
+    # add_exception_handlers=True,#生产环境不要开，会泄露调试信息
+)
 app.mount('/statics', StaticFiles(directory='statics'), name='statics')
+
+
 
 for router in routers:
     app.include_router(router["route"], prefix=router["prefix"], tags=router["tags"])
