3fd5a3c4a75e6451fc5805b311c1991f9f72e820
wu-yiqiang <wu_yiqiang@outlook.com>
2024-07-04 01:28:10 +0800

    新建driver表

--- migrations/app/0_20240704011848_init.py
+++ migrations/app/0_20240704011848_init.py
@@ -33,6 +33,16 @@ CREATE TABLE IF NOT EXISTS `truck` (
     `displayName` VARCHAR(255) NOT NULL UNIQUE,
     `contractorsId_id` CHAR(36) NOT NULL,
     CONSTRAINT `fk_truck_contract_eed508f7` FOREIGN KEY (`contractorsId_id`) REFERENCES `contractor` (`id`) ON DELETE CASCADE
+) CHARACTER SET utf8mb4;
+CREATE TABLE IF NOT EXISTS `driver` (
+    `id` CHAR(36) NOT NULL  PRIMARY KEY,
+    `created_at` DATETIME(6) NOT NULL  DEFAULT CURRENT_TIMESTAMP(6),
+    `updated_at` DATETIME(6) NOT NULL  DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6),
+    `is_deleted` BOOL NOT NULL  DEFAULT 0,
+    `givenName` VARCHAR(255) NOT NULL UNIQUE,
+    `gender` BOOL NOT NULL UNIQUE,
+    `contractorsId_id` CHAR(36) NOT NULL,
+    CONSTRAINT `fk_driver_contract_a5f42020` FOREIGN KEY (`contractorsId_id`) REFERENCES `contractor` (`id`) ON DELETE CASCADE
 ) CHARACTER SET utf8mb4;"""
 
 
