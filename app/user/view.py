a629fa59c56bf66b74f04bdcc494fed79274b7df
wu-yiqiang <wu_yiqiang@outlook.com>
2024-07-22 23:59:03 +0800

    登录接口token支持

--- app/user/view.py
+++ app/user/view.py
@@ -1,22 +1,36 @@
-from fastapi import APIRouter, Query, status
+from fastapi import APIRouter, Query, status,Depends
 from typing import Union
-from common import response
 from app.user.serialize import UserForm
 from app.user.models import Users
 from app.common.response import common_response
 from utils.encrypt import encryptStr, decryptStr,keyValue
+from aioredis import Redis
+from db.redis import sys_cache
+from utils.jwt import create_access_token
 # 实例化APIRouter实例
 userrouter = APIRouter()
 
 
 # 注册具体方法
 @userrouter.post("/login")
-async def login(login: UserForm):
-    user = await Users.filter(is_deleted = 0, email = login.email, password = login.password).first().values('id', 'email', 'uuid','updated_at','created_at', 'roles__id', 'menus__id', 'buttons__id', 'routers__id')
+async def login(login: UserForm, cache: Redis = Depends(sys_cache)):
+    password = decryptStr(login.email, login.password)
+    newPassword = encryptStr(keyValue, password)
+    user = await Users.filter(is_deleted = 0, email = login.email, password = newPassword).first().values('id', 'email', 'uuid','updated_at','created_at', 'roles__id', 'menus__id', 'buttons__id', 'routers__id')
     if user is None:
         return common_response(10000000)
-    # 生成token 设置token
-    return common_response(200, user)
+     # 查询token是否存在
+    token = await cache.get(name=login.email)
+    data = dict()
+    data = user
+    if token is None:
+        # 生成token 设置token
+        createToken = create_access_token(login.email)
+        await cache.set(name=login.email, value=createToken)
+        data['token'] = createToken
+    else:
+       data['token'] = token
+    return common_response(200, data)
 
 
 @userrouter.post("/register", status_code=status.HTTP_200_OK)
---
+++ utils/jwt.py
@@ -0,0 +1,33 @@
+import os
+from datetime import datetime, timedelta
+from typing import Union, Any
+import jwt
+
+ACCESS_TOKEN_EXPIRE_MINUTES = 30  # 30 minutes
+REFRESH_TOKEN_EXPIRE_MINUTES = 60 * 24 * 7 # 7 days
+ALGORITHM = "HS256"
+JWT_SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"   # should be kept secret
+JWT_REFRESH_SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"    # should be kept secret
+
+
+def create_access_token(subject: Union[str, Any], expires_delta: int = None) -> str:
+    if expires_delta is not None:
+        expires_delta = datetime.utcnow() + expires_delta
+    else:
+        expires_delta = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+
+    to_encode = {"exp": expires_delta, "sub": str(subject)}
+    encoded_jwt = jwt.encode(to_encode, JWT_SECRET_KEY, ALGORITHM)
+    return encoded_jwt
+
+
+def create_refresh_token(subject: Union[str, Any], expires_delta: int = None) -> str:
+    if expires_delta is not None:
+        expires_delta = datetime.utcnow() + expires_delta
+    else:
+        expires_delta = datetime.utcnow() + timedelta(minutes=REFRESH_TOKEN_EXPIRE_MINUTES)
+
+    to_encode = {"exp": expires_delta, "sub": str(subject)}
+    encoded_jwt = jwt.encode(to_encode, JWT_REFRESH_SECRET_KEY, ALGORITHM)
+    return encoded_jwt
+
