72d060b6add89b5b4426953eddc49e8768a62161
wu-yiqiang <wu_yiqiang@outlook.com>
2024-07-31 00:12:52 +0800

    人脸识别

--- utils/FaceRecognition.py
+++ utils/FaceRecognition.py
@@ -1,2 +1,19 @@
-# import cv2
-# import numpy as np
+import cv2 as cv
+def DetectFaces():
+    grey_img = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+    detect_face = cv.CascadeClassifier('/Users/atlas/fastAPI_py/.venv/lib/python3.9/site-packages/cv2/data/haarcascade_frontalface_default.xml')
+    face = detect_face.detectMultiScale(grey_img, 1.01, 5,0,(100,100),(300, 300))
+    for (x, y, w, h) in face:
+        cv.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), 2)
+    cv.imshow('result', img)
+
+
+img = cv.imread('../images/face1.jpg')
+DetectFaces()
+
+while True:
+    if ord('q') == cv.waitKey(0):
+        break
+
+cv.destroyAllWindows()
+
