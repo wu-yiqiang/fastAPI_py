115b5440e416c80bd53a7cb2e1f417c091b6e0da
wu-yiqiang <wu_yiqiang@outlook.com>
2024-08-02 01:11:43 +0800

    人脸识别

--- utils/FaceRecognition.py
+++ utils/FaceRecognition.py
@@ -53,26 +53,19 @@ def getImageAndLabels(path: str, label: str):
 # names = []
 
 #准备识别的图片
-def face_detect_demo(img, name):
+def face_detect(img, name):
     recogizer = cv2.face.LBPHFaceRecognizer.create()
     recogizer.read(facedata_path()+name+'.yml')
     gray=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)#转换为灰度
     face_detector= cv2.Cascadeclassifier(classifiers_path())
     face=face_detector.detectMultiScale(gray, 1.1, 5, cv2.CASCADE_SCALE_IMAGE, (100, 100), (300, 300))
     for x,y, w, h in face:
-        cv2.rectangle(img,(x,y),(x+w,y+h),color=(0,0,255),thickness=2)
-        cv2.circle(img, center=(x+w//2, y+h//2), radius=w//2, color=(0, 255,0), thickness=1) #人脸识别
         ids, confidence = recogizer.predict(gray[y: y+h, x: x+w])
-        #print('标签id:',ids,'置信评分：'，confidence)
-        if confidence >80:
-            global warningtime
-            warningtime + 1
-            if warningtime > 100:
-                warningtime = 0
-            cv2.putText(img, 'unkonw', (x + 10, y -10), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (0, 255, 0), 1)
+        # print('标签id:',ids,'置信评分：'，confidence)
+        if confidence > 80:
+            return False
         else:
-            cv2.putText(img, str(names[ids-1]), (x + 10, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (0, 255, 0), 1)
-    cv2.imshow( 'result',img)
+            return True
 
 
 
